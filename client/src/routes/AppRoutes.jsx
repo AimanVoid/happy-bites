22eda749e345d3c4aa0b6e89b93da00370107e01
Aiman Maroof <aiman.maroofofficial@gmail.com>
2026-08-02 18:30:20 -0700

    Initial MERN project setup

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MainLayout from "../layouts/MainLayout";
+
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Wishlist from "../pages/Wishlist";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
